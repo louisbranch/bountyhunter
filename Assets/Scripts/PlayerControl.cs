9a558c563fdad80107a9aff4c0106181aa1836f6
Luiz Branco <me@luizbranco.com>
2015-01-17 11:38:17 -0500

    Add initial project structure

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float speed = 1.0f;
+
+	private bool facingRight = false;
+	
+	private void Update () {
+		float hMove = Input.GetAxis("Horizontal");
+		float vMove = Input.GetAxis("Vertical");
+
+		if (hMove != 0) {
+			transform.Translate(Vector2.right * hMove * speed * Time.deltaTime);
+			if (hMove > 0 && !facingRight) {
+				Flip();
+			} else if (hMove < 0 && facingRight) {
+				Flip();
+			}
+		}
+
+		if (vMove != 0) {
+			transform.Translate(Vector2.up * vMove * speed * Time.deltaTime);
+		}
+	}
+
+	private void Flip() {
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+		facingRight = !facingRight;
+	}
+}
