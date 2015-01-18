0edc2451bf5a0e632115955c65e3a664d3b63e4b
Luiz Branco <me@luizbranco.com>
2015-01-18 11:02:19 -0500

    Initial collision detection

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,24 +4,60 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour {
 
 	public float speed = 1.0f;
+	public LayerMask impassibleLayer;
+	public float checkerRadius = 0.05f;
 
 	private bool facingRight = false;
+
+	private bool moveN = true;
+	private bool moveE = true;
+	private bool moveS = true;
+	private bool moveW = true;
+
+	Transform north;
+	Transform east;
+	Transform south;
+	Transform west;
+
+	private void Awake() {
+		north = transform.Find("North");
+		east = transform.Find("East");
+		south = transform.Find("South");
+		west = transform.Find("West");
+	}
 	
 	private void Update () {
 		float hMove = Input.GetAxis("Horizontal");
 		float vMove = Input.GetAxis("Vertical");
 
-		if (hMove != 0) {
-			transform.Translate(Vector2.right * hMove * speed * Time.deltaTime);
-			if (hMove > 0 && !facingRight) {
+		moveN = !Physics2D.OverlapCircle(north.position, checkerRadius, impassibleLayer);
+		moveE = !Physics2D.OverlapCircle(east.position, checkerRadius, impassibleLayer);
+		moveS = !Physics2D.OverlapCircle(south.position, checkerRadius, impassibleLayer);
+		moveW = !Physics2D.OverlapCircle(west.position, checkerRadius, impassibleLayer);
+
+
+		if (hMove > 0) {
+			Debug.Log (moveE);
+			if (moveE) {
+				transform.Translate(Vector2.right * speed * Time.deltaTime);
+			}
+
+			if (!facingRight) { 
 				Flip();
-			} else if (hMove < 0 && facingRight) {
+			}
+		} else if (hMove < 0) {
+			if (moveW) {
+				transform.Translate(-Vector2.right * speed * Time.deltaTime);
+			}
+			if (facingRight) {
 				Flip();
 			}
 		}
 
-		if (vMove != 0) {
-			transform.Translate(Vector2.up * vMove * speed * Time.deltaTime);
+		if (vMove > 0 && moveN) {
+			transform.Translate(Vector2.up * speed * Time.deltaTime);
+		} else if (vMove < 0 && moveS) {
+			transform.Translate(-Vector2.up * speed * Time.deltaTime);
 		}
 	}
 
