ead69b5af4ceaaf63fd7f61147602ae8583682f2
Wahid Shafique <wahidshafique@hotmail.com>
2015-01-24 14:10:01 -0500

    Added scenes and prefabs
    improved 2 scenes, made the walls prefabs for deck 3. Added fire trigger
    (preliminary). Will organize and segment things into more prefabs later.

---
+++ Assets/Scripts/FireTriggerEnter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireTriggerEnter : MonoBehaviour {
+	// Use this for initialization
+	Animator anim;
+
+	void Start () {
+		anim=GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D (Collider2D other){
+		Debug.Log ("hit fire");
+		if (other.tag=="Fire"){
+			anim.SetBool("isOnFire", true);
+		}
+}
+}
