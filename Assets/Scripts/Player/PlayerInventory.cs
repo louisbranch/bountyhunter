26f6f2161db351278957dbe04f5c4d397377091f
Luiz Branco <me@luizbranco.com>
2015-01-23 22:34:42 -0500

    Allow player to pickup weapon

---
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInventory : MonoBehaviour {
+
+	private GameObject weapon;
+
+	Animator anim;
+
+	private void Awake() {
+		anim = GetComponent<Animator>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D coll) {
+		switch (coll.tag) {
+			case "Weapon":
+			weapon = coll.gameObject;
+			anim.CrossFade("SoldierW" + coll.name, 0f);
+			Destroy(coll.gameObject);
+			break;
+		}
+	}
+}
