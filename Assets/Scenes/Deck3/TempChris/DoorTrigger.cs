0afc40073ad71796f5eb4b6181c67c7aa0fc4f75
Christopher Aytona <christopher.aytona@hotmail.com>
2015-01-30 13:49:10 -0500

    Door Prefab

---
+++ Assets/Scenes/Deck3/TempChris/DoorTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTrigger : MonoBehaviour {
+	Animator anim;
+	
+	void Start () {
+		anim=GetComponent<Animator>();
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.tag=="Player"){
+			anim.SetTrigger("OpenDoor");
+			//collider.enabled = false;
+		} 
+	}
+}
