c41370e8bea88b503c7d750367b21a43f5238ccd
aytona <christopher.aytona@hotmail.com>
2015-02-02 16:51:54 -0500

    Added floors

--- Assets/Scripts/Items/DoorTrigger.cs
+++ Assets/Scripts/Items/DoorTrigger.cs
@@ -5,11 +5,11 @@ public class DoorTrigger : MonoBehaviour {
 	Animator anim;
 	
 	void Start () {
-		anim=GetComponent<Animator>();
+		anim = GetComponent<Animator>();
 	}
 
 	void OnTriggerEnter2D (Collider2D other){
-		if (other.tag=="Player"){
+		if (other.tag == "Player"){
 			anim.SetTrigger("OpenDoor");
 			//collider.enabled = false;
 		} 
