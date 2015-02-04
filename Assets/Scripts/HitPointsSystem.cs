275d3533a600d6528f54fd99e8279fdad8ccfcd2
Luiz Branco <me@luizbranco.com>
2015-02-03 19:43:21 -0500

    Add hp system

---
+++ Assets/Scripts/HitPointsSystem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitPointsSystem : MonoBehaviour {
+
+	public float health;
+
+	private void OnTriggerEnter2D(Collider2D coll) {
+		switch (coll.gameObject.tag) {
+		case "Projectile":
+			health--;
+			if (health <= 0) {
+				Destroy(gameObject);
+			}
+			break;
+		}
+	}
+
+}
