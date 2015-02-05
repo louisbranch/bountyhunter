83a19c59856ce0247e960b0db2f2f0dd4d84c541
Luiz Branco <me@luizbranco.com>
2015-02-05 01:55:59 -0500

    Initial enemy ai file

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+
+	private void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.tag == "Player") {
+			Debug.Log("ENEMY!");
+		}
+	}
+
+}
