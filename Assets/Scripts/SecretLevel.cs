2de1da21414c8ebcf0a176869e36413618ad6b76
Christopher Aytona <christopher.aytona@hotmail.com>
2015-02-09 08:28:02 -0500

    SecretLevel script moved, working on stealth door

--- Assets/Scripts/SecretLevel.cs
+++ Assets/Scripts/SecretLevel.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class SecretLevel : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D other){
-		if (other.tag == "FloorTrigger")
+		if (other.tag == "Player")
 		{
 			Application.LoadLevel("TestScene");
 		}
--- Assets/Scripts/StealthDoor.cs
+++ Assets/Scripts/StealthDoor.cs
@@ -5,17 +5,27 @@ public class StealthDoor : MonoBehaviour {
 
 	public GameObject OpenDoor;
 	public GameObject ClosedDoor;
+	public float CountdownTime = 0f;
 
 	void Awake(){
 		OpenDoor.SetActive (false);
 		ClosedDoor.SetActive (true);
 		}
 
-	void OnTriggerEnter2D(Collider2D coll)
+	void OnTriggerEnter2D(Collider2D other)
 	{
-		if (Input.GetKey (KeyCode.E)) {
+		if (other.tag == "Player") {
 				ClosedDoor.SetActive (false);
 				OpenDoor.SetActive (true);
+
 		}
+	}
+
+	void Update(){
+						CountdownTime -= Time.deltaTime;
+						if (CountdownTime <= 0) {
+								OpenDoor.SetActive (false);
+								ClosedDoor.SetActive (true);
+						}
 	}
 }
