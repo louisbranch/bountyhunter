2243b8a28458f0a9e91a8203510717001dfa4ae3
Wahid Shafique <wahidshafique@hotmail.com>
2015-02-09 21:35:24 -0500

    Playing with texture map and made minor scene edits
    Instructions were not clear, now the test scene loads the giant space
    maze cathedral of the Great Khan. Revel in his frothy glory!!!

---
+++ Assets/Scenes/TestTileScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestTileScript : MonoBehaviour {
+	
+	private int width;
+	private int height;
+	
+	public Transform floorTile;
+	public Transform wallTile;
+	
+	private Color[] tileColors;
+	
+	public Color tileColor;
+	public Color wallColor;
+	
+	
+	public Texture2D levelTexture;
+	
+	//public PlayerControl player;
+	
+	void Start () {
+		width = levelTexture.width;
+		height = levelTexture.height;
+		loadLevel();
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+	void loadLevel()
+	{
+		tileColors = new Color[width * height];
+		tileColors = levelTexture.GetPixels();
+		
+		for (int y = 0; y < height; y++)
+		{
+			
+			for (int x = 0; x < width; x++)
+			{
+				
+				
+				if(tileColors[x+y*width] == tileColor)
+				{
+					Instantiate(floorTile, new Vector3(x, y), Quaternion.identity);
+				}
+				if(tileColors[x+y*width] == wallColor)
+				{
+					Instantiate(wallTile, new Vector3(x, y), Quaternion.identity);
+					
+				}
+			}
+		}
+	}
+}
