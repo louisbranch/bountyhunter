01a2ce79b9972525cdac7d1e66cfe3e1619fcc69
Christopher Aytona <christopher.aytona@hotmail.com>
2015-02-13 11:42:56 -0500

    Fixed BossScene Doors

---
+++ Assets/Scripts/BossLevel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossLevel : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D target) {
+		if (target.tag == "Player") {
+			Application.LoadLevel ("BossScene");
+		}
+	}
+}
---
+++ Assets/Scripts/StealthLevel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StealthLevel : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D target) {
+		if (target.tag == "Player") {
+			Application.LoadLevel ("StealthScene");
+		}
+	}
+}
