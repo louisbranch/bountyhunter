903e9954c248d7ef7b7f89910b424c895ef21f1e
Christopher Aytona <christopher.aytona@hotmail.com>
2015-02-13 09:28:26 -0500

    Open door now works by pressing E
    - This destroys the door

--- Assets/Scripts/StealthDoor.cs
+++ Assets/Scripts/StealthDoor.cs
@@ -3,29 +3,26 @@ using System.Collections;
 
 public class StealthDoor : MonoBehaviour {
 
-	public GameObject OpenDoor;
-	public GameObject ClosedDoor;
-	public float CountdownTime = 0f;
+	public bool NearDoor = false;
 
-	void Awake(){
-		OpenDoor.SetActive (false);
-		ClosedDoor.SetActive (true);
-		}
-
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		if (other.tag == "Player") {
-				ClosedDoor.SetActive (false);
-				OpenDoor.SetActive (true);
-
+	void Update() {
+		if (NearDoor == true) {
+			if (Input.GetKey(KeyCode.E)) {
+				Destroy(this.gameObject);
+			
+			}
 		}
 	}
 
-	void Update(){
-						CountdownTime -= Time.deltaTime;
-						if (CountdownTime <= 0) {
-								OpenDoor.SetActive (false);
-								ClosedDoor.SetActive (true);
-						}
+	void OnTriggerEnter2D (Collider2D target) {
+		if (target.tag == "Player") {
+			NearDoor = true;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D target) {
+		if (target.tag == "Player") {
+			NearDoor = false;
+		}
 	}
 }
